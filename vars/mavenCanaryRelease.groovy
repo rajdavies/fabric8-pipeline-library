627468f4d36850daf7aa0517a4438d1d8c5c6f82
Rob <rajdavies@gmail.com>
2017-03-16 08:05:21 +0000

    added some extra output

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -45,7 +45,11 @@ def call(body) {
             def user = groupId[groupId.size()-1].trim()
             def artifactId = m.artifactId
             echo "running docker tag ..."
-            sh "docker tag ${user}/${artifactId}:${config.version} ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${artifactId}:${config.version}"
+            def command = "docker tag ${user}/${artifactId}:${config.version} ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${artifactId}:${config.version}"
+
+            def out = sh script: command,returnStdOut:true
+            echo "result for docker tag = ${out}"
+            //sh "docker tag ${user}/${artifactId}:${config.version} ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${artifactId}:${config.version}"
 
         }else{
             retry(3){
