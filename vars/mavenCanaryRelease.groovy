9e505145d7e19d37ed0173e578574189dee9e70b
Rob <rajdavies@gmail.com>
2017-03-16 07:09:08 +0000

    added some extra output

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -44,6 +44,7 @@ def call(body) {
             def groupId = m.groupId.split( '\\.' )
             def user = groupId[groupId.size()-1].trim()
             def artifactId = m.artifactId
+            echo "running docker tag ..."
             sh "docker tag ${user}/${artifactId}:${config.version} ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${artifactId}:${config.version}"
 
         }else{
